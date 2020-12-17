14139fac940bbe7283f1f793ba3c036a233291b3
rushtehrani <r@onepanel.io>
2020-12-16 19:36:58 -0800

    add port argument to nni script

--- workflows/nni/start.py
+++ workflows/nni/start.py
@@ -5,7 +5,7 @@ def main(args):
     exp = Experiment()
     
     print(args.config)
-    exp.start_experiment(args.config)
+    exp.start_experiment(args.config, port=args.port)
     
     status = exp.get_experiment_status()
     while status['status'] != 'DONE':
@@ -15,6 +15,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=9000, help='NNI port')
     required_argument_group = parser.add_argument_group('required arguments')
     required_argument_group.add_argument('--config', help='NNI configuration', required=True)
 
