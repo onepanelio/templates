44c625db63cbf9a4507fc64045ac35be75c8cb6c
rushtehrani <r@onepanel.io>
2020-12-17 22:16:17 -0800

    use keras sequential model for nni example

--- workflows/nni-hyperparameter-tuning/mnist/mnist.py
+++ workflows/nni-hyperparameter-tuning/mnist/mnist.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
 """
 NNI example trial code.
 
@@ -24,46 +21,6 @@ import nni
 _logger = logging.getLogger('mnist_example')
 _logger.setLevel(logging.INFO)
 
-
-class MnistModel(Model):
-    """
-    LeNet-5 Model with customizable hyper-parameters
-    """
-    def __init__(self, conv_size, hidden_size, dropout_rate):
-        """
-        Initialize hyper-parameters.
-
-        Parameters
-        ----------
-        conv_size : int
-            Kernel size of convolutional layers.
-        hidden_size : int
-            Dimensionality of last hidden layer.
-        dropout_rate : float
-            Dropout rate between two fully connected (dense) layers, to prevent co-adaptation.
-        """
-        super().__init__()
-        self.conv1 = Conv2D(filters=32, kernel_size=conv_size, activation='relu')
-        self.pool1 = MaxPool2D(pool_size=2)
-        self.conv2 = Conv2D(filters=64, kernel_size=conv_size, activation='relu')
-        self.pool2 = MaxPool2D(pool_size=2)
-        self.flatten = Flatten()
-        self.fc1 = Dense(units=hidden_size, activation='relu')
-        self.dropout = Dropout(rate=dropout_rate)
-        self.fc2 = Dense(units=10, activation='softmax')
-
-    def call(self, x):
-        """Override ``Model.call`` to build LeNet-5 model."""
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.dropout(x)
-        return self.fc2(x)
-
-
 class ReportIntermediates(Callback):
     """
     Callback class for reporting intermediate accuracy metrics.
@@ -76,11 +33,7 @@ class ReportIntermediates(Callback):
     """
     def on_epoch_end(self, epoch, logs=None):
         """Reports intermediate accuracy to NNI framework"""
-        # TensorFlow 2.0 API reference claims the key is `val_acc`, but in fact it's `val_accuracy`
-        if 'val_acc' in logs:
-            nni.report_intermediate_result(logs['val_acc'])
-        else:
-            nni.report_intermediate_result(logs['val_accuracy'])
+        nni.report_intermediate_result(logs['val_accuracy'])
 
 
 def load_dataset():
@@ -115,28 +68,37 @@ def save_best_metrics(loss, accuracy):
     with open('/tmp/sys-metrics.json', 'w') as f:
         json.dump(metrics, f)
 
-    _logger.info('Saved best metrics.')
+    _logger.info('Best metrics saved')
     return True
 
 def main(params):
     """
     Main program:
-      - Build network
       - Prepare dataset
+      - Build network
       - Train the model
       - Report accuracy to tuner
+      - Save best current metrics
+      - Save best current model
     """
-    model = MnistModel(
-        conv_size=params['conv_size'],
-        hidden_size=params['hidden_size'],
-        dropout_rate=params['dropout_rate']
-    )
-    optimizer = Adam(learning_rate=params['learning_rate'])
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    _logger.info('Model built')
 
     (x_train, y_train), (x_test, y_test) = load_dataset()
     _logger.info('Dataset loaded')
+    
+    model = tf.keras.Sequential([
+          tf.keras.layers.Conv2D(filters=32, kernel_size=params['conv_size'], activation='relu'),
+          tf.keras.layers.MaxPool2D(pool_size=2),
+          tf.keras.layers.Conv2D(filters=64, kernel_size=params['conv_size'], activation='relu'),
+          tf.keras.layers.MaxPool2D(pool_size=2),
+          tf.keras.layers.Flatten(),
+          tf.keras.layers.Dense(units=params['hidden_size'], activation='relu'),
+          tf.keras.layers.Dropout(rate=params['dropout_rate']),
+          tf.keras.layers.Dense(units=10, activation='softmax')
+      ])
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=params['learning_rate']),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    _logger.info('Model built')
 
     model.fit(
         x_train,
@@ -158,7 +120,8 @@ def main(params):
 
     # save the model if accuracy is better than previous model
     if is_best_accuracy:
-        model.save('/mnt/output')
+        model.save('/mnt/output/model.h5')
+        _logger.info('Best model saved')
 
 
 if __name__ == '__main__':
@@ -171,7 +134,6 @@ if __name__ == '__main__':
     }
 
     # fetch hyper-parameters from HPO tuner
-    # comment out following two lines to run the code without NNI framework
     tuned_params = nni.get_next_parameter()
     params.update(tuned_params)
 
