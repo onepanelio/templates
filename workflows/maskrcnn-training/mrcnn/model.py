ba2c65d099a5610b39da18ecd17c07582651cf8a
Gonzalo Tixilima <javoweb@msn.com>
2021-01-12 18:59:57 -0500

    fix disable eager execution

--- workflows/maskrcnn-training/mrcnn/model.py
+++ workflows/maskrcnn-training/mrcnn/model.py
@@ -26,8 +26,9 @@ from mrcnn import utils
 
 # Requires TensorFlow 1.3+ and Keras 2.0.8+.
 from distutils.version import LooseVersion
-assert LooseVersion(tf.__version__) >= LooseVersion("1.3")
-assert LooseVersion(keras.__version__) >= LooseVersion('2.0.8')
+assert LooseVersion(tf.__version__) >= LooseVersion("2.1")
+
+tf.compat.v1.disable_eager_execution()
 
 
 ############################################################
