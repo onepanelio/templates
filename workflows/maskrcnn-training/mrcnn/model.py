ffd9506e095aa90b2c1062425dea774045838d5d
Gonzalo Tixilima <javoweb@msn.com>
2021-01-20 16:42:01 -0500

    add use default RaggedTensorSpec

--- workflows/maskrcnn-training/mrcnn/model.py
+++ workflows/maskrcnn-training/mrcnn/model.py
@@ -2334,8 +2334,8 @@ class MaskRCNN(object):
 
         # Data generators
         train_generator = tf.data.Dataset.from_generator(DataGenerator(train_dataset, self.config, shuffle=True,
-                                         augmentation=augmentation))
-        val_generator = tf.data.Dataset.from_generator(DataGenerator(val_dataset, self.config, shuffle=True))
+                                         augmentation=augmentation), tf.RaggedTensorSpec())   
+        val_generator = tf.data.Dataset.from_generator(DataGenerator(val_dataset, self.config, shuffle=True), tf.RaggedTensorSpec())
 
         # Create log_dir if it does not exist
         if not os.path.exists(self.log_dir):
