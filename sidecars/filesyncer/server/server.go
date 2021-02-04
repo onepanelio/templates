86c88c26be8f843993bbe459252ffec4e2cc6425
rushtehrani <r@onepanel.io>
2021-02-03 17:40:02 -0800

    fix json tag value

--- sidecars/filesyncer/server/server.go
+++ sidecars/filesyncer/server/server.go
@@ -94,10 +94,10 @@ func sync() http.Handler {
 		w.Header().Set("content-type", "application/json")
 
 		result := struct {
-			Message string `json:"message"`
-			Timestamp int64 `json:"start"`
-		} {
-			Message: "Sync command sent",
+			Message   string
+			Timestamp int64
+		}{
+			Message:   "Sync command sent",
 			Timestamp: timestamp,
 		}
 
